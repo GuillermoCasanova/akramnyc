435a54d86a8e8324d74d1353a7ace58471fd8f4c
Guillermo Casanova <Casanova@MacBook-Pro.local>
2021-08-10 23:38:31 -0400

    + Inserting klayvio later in the chain

--- assets/cs-navigation.js
+++ assets/cs-navigation.js
@@ -28,6 +28,12 @@ class ComingSoonNav extends HTMLElement {
 
         let navigationAnimation = gsap.timeline({onComplete: function() {
             that.setUpNavigation(camera); 
+            that.loadScript("https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=RSpZfU")
+            .then((data) => {
+            })
+            .catch((err) => {
+                console.error(err);
+            });
         }}); 
 
         navigationAnimation.fromTo('.navigation__links', {
@@ -160,6 +166,34 @@ class ComingSoonNav extends HTMLElement {
     });
     
     return page;
+   }
+
+   loadScript(src, async = true, type="text/javascript") {
+    return new Promise((resolve, reject) => {
+        try {
+            const el = document.createElement("script");
+            const container = document.head || document.body;
+
+            el.type = type;
+            el.async = async;
+            el.src = src;
+
+            el.addEventListener("load", () => {
+                resolve({ status: true });
+            });
+
+            el.addEventListener("error", () => {
+                reject({
+                    status: false,
+                    message: `Failed to load the script ${src}`
+                });
+            });
+
+            container.appendChild(el);
+        } catch (err) {
+            reject(err);
+        }
+    });
    }
 
 
