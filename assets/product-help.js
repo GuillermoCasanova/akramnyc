556c64e2e8e51b60ccb1d331a1d3f76d590ab9d3
Guillermo Casanova <Casanova@MacBook-Pro.local>
2021-11-05 01:03:30 -0400

    + connecting product support with CMS on PDP page

--- assets/product-help.js
+++ assets/product-help.js
@@ -5,11 +5,26 @@
 class ProductHelp extends HTMLElement {
     constructor() {
         super() 
+        this.mediaQueries = {
+            mediumDown: window.matchMedia('(max-width: 974px)'),
+            largeUp: window.matchMedia('(min-width: 975px)')
+        }
+        
         this.init(); 
     }
 
     init() {
-        this.setUpEvents(); 
+        this.mediaQueries.largeUp.addEventListener("change", this.handleLargeUp.bind(this)); 
+        this.mediaQueries.mediumDown.addEventListener("change", this.handleMediumDown.bind(this)); 
+
+        if(this.mediaQueries.mediumDown.matches) {
+            console.log('DOWN');
+            this.handleMediumDown(this.mediaQueries.mediumDown); 
+        }
+
+        if(this.mediaQueries.largeUp.matches) {
+            this.handleLargeUp(this.mediaQueries.largeUp); 
+        }
     }
 
     openModal(pId) {
@@ -20,21 +35,39 @@ class ProductHelp extends HTMLElement {
         this.querySelector('aside').classList.remove('is-visible');
     }
 
-    setUpEvents() { 
-        this.querySelectorAll('[data-help-toggle]').forEach((elem) => {
-            console.log(elem);
-            elem.addEventListener('click', () =>  {
-                document.querySelector('.product__info-wrapper').style.zIndex = 100; 
-                this.openModal(this.dataset.id);
-            }); 
-        })
+    handleMediumDown(pEvent) {
 
-        this.querySelectorAll('[data-help-close]').forEach((elem) => {
-            elem.addEventListener('click', (event) =>  {
-                document.querySelector('.product__info-wrapper').style.zIndex = 0;
-                this.closeModal(event);
-            }); 
-        })
+    }   
+
+    handleMediumDown(pEvent) {
+        if(pEvent.matches) {
+
+
+
+        }
+    }
+    
+    handleLargeUp(pEvent) {
+
+        if(pEvent.matches) {
+            this.querySelectorAll('[data-help-toggle]').forEach((elem) => {
+                elem.addEventListener('click', () =>  {
+                    document.querySelector('.product__info-wrapper').style.zIndex = 100; 
+                    this.openModal(this.dataset.id);
+                }); 
+            })
+
+            this.querySelectorAll('[data-help-close]').forEach((elem) => {
+                elem.addEventListener('click', (event) =>  {
+                    document.querySelector('.product__info-wrapper').style.zIndex = 0;
+                    this.closeModal(event);
+                }); 
+            })
+        }
+    }   
+
+    setUpEvents() { 
+    
     }
 
 }
